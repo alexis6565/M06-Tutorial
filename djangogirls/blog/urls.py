8b090cbcb629544fa391f0dd0c153cd5a83f66b9
Alexis Phillips <aphillips225@ivytech.edu>
2025-07-19 23:50:23 -0400

    M06 tutorial

---
+++ djangogirls/blog/urls.py
@@ -0,0 +1,21 @@
+#importing django path function and all views from the blog application 
+
+from django.urls import path 
+from . import views 
+
+# view is a place where we put "logic"of our application 
+# views are placed in the views.py file in the blog folder 
+#assigning a view called post list to the root URL
+#URL pattern will match an empty string and the Django URL resolver will ignore the domain name that prefixes the full URL path 
+#name post_list is url name that will ID the view and can be the same or different name from the view
+
+#first url pattern
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]
+
+#post/<int:pk>/ specifies a URL pattern – we will explain it for you:
+#post/ means that the URL should begin with the word post followed by a /. So far so good.
+#<int:pk> – this part is trickier. It means that Django expects an integer value and will transfer it to a view as a variable called pk.
+#/ – then we need a / again before finishing the URL.
---
+++ djangogirls/blog/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, get_object_or_404  
+from django.utils import timezone
+#include the code from model in models.py by typing the code on line 3
+from .models import Post #the dot before models means current directory or application
+
+# Create your views here.
+#views connect models and templates 
+#need to take the models we want to display in our post_list view and pass them to the template 
+
+
+def post_list(request,):
+    #create a variable for our query set called posts/ name of queryset = posts
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts}) #render function has one parameter = request(everything from the user via the internet) and another giving the template file ('blog/post_list.html')
+#created a def function called post_list that takes request and will return the value it gets from calling render function 
+#render function will put together our template 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+#go back to template and display the queryset
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
